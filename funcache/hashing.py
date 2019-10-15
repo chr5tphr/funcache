2bd6c4c399c415c70a783fca4cbd16122fc658fa
chrstphr <chrstphr@posteo.eu>
2019-10-15 13:23:22 +0200

    persistent id for numpy now is based on full data again, not only mean and std

--- funcache/hashing.py
+++ funcache/hashing.py
@@ -1,5 +1,6 @@
 import pickle
 
+import numpy as np
 from metrohash import MetroHash128
 
 try:
@@ -14,22 +15,29 @@ except ImportError:
 
 
 class Hasher(MetroHash128):
+    """Hasher object with a write function for file-like updates"""
     def write(self, data):
+        """Update using write for file-like behaviour"""
         self.update(data)
         return len(data)
 
 
 class HashPickler(pickle.Pickler):
+    """Pickler for computing Hashes"""
     @staticmethod
     def numpy_id(obj):
+        """Persistent id for numpy arrays"""
+        mantissa, exponent = np.frexp(obj)
+        np.around(mantissa, decimals=2, out=mantissa)
         return (
             obj.dtype.name,
             obj.shape,
-            '{:.2e}'.format(obj.mean()),
-            '{:.2e}'.format(obj.std()),
+            bytes(mantissa),
+            bytes(exponent),
         )
 
     def persistent_id(self, obj):
+        """Persistent ids for persistent pickles"""
         if isinstance(obj, ndarray):
             return self.numpy_id(obj)
         elif isinstance(obj, Tensor):
@@ -39,6 +47,7 @@ class HashPickler(pickle.Pickler):
 
 
 def ext_hash(data):
+    """Extended non-cryptographic Hashing using Pickle and MetroHash"""
     hasher = Hasher()
     HashPickler(hasher).dump(data)
     return hasher.hexdigest()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "funcache",
-    version = "0.1.1",
+    version = "0.1.2",
     packages=find_packages(),
     install_requires=[
         'metrohash-python>=1.1.3.post2',
